544a031a9bb2e6f5a1c4e43f451b783172d72587
juliengrima <gogrinformatique@gmail.com>
2025-01-26 16:05:50 +0100

    chrono

--- Assets/Bin/Scripts/UserInterfaces/UIManager.cs
+++ Assets/Bin/Scripts/UserInterfaces/UIManager.cs
@@ -21,32 +21,42 @@ namespace Manager
         [Header("Bars Informations")]
         [SerializeField] Image _healthbar;
         [SerializeField] Image _powerbar;
+        [SerializeField] TMP_Text _text;
+        [Header("Message")]
+        [SerializeField] GameObject _message;
+        [Header("Scene")]
+        [SerializeField] string _menuName;
         //PRIVATE
         //PUBLIC
+        public static UIManager Instance;
         #endregion
         #region Default Informations
         void Reset()
         {
-        
+            _menuName = "Menu";
         }
         #endregion
         #region Unity LifeCycle
         // Start is called before the first frame update
-    
-        void Awake()
+        private void Awake()
         {
-        
+            if (Instance == null)
+            {
+                //Debug.Log("InputManager is single");
+                Instance = this;
+            }
+            else
+            {
+                Debug.Log("UIManager already exist");
+                Destroy(this);
+            }
         }
-        void Start()
-        {
-        
-        }
-
         // Update is called once per frame
         void Update()
         {
             UpdateHealthBar();
             UpdatePowerBar();
+            Chrono();
         }
         #endregion
         #region Methods
@@ -76,6 +86,33 @@ namespace Manager
                 //_currentHealthText.text = Mathf.CeilToInt(_scriptablePlayer.CurrentHealth).ToString();
                 //_maxHealthText.text = Mathf.CeilToInt(_scriptablePlayer.MaxHealth).ToString();
             }
+        }
+
+        public void Chrono()
+        {
+            if (PlayerTimer.Instance.ElapsedTime == 0)
+            {
+                _text.text = "0";
+            }
+
+            TimeSpan _timeSpan = TimeSpan.FromSeconds(PlayerTimer.Instance.ElapsedTime);
+            //String Format ("{0=Parametre:00=time}:{1:00}, Parametre_0", Parametre_1)
+            _text.text = string.Format("{0:00}:{1:00}:{2:00}:{3:000}",
+                                                            _timeSpan.Hours,
+                                                            _timeSpan.Minutes,
+                                                            _timeSpan.Seconds,
+                                                            _timeSpan.Milliseconds);
+        }
+        #endregion
+        #region EndLevel
+        public void EnableWinmessage()
+        {
+            _message.SetActive(true);
+        }
+
+        public void ExitToMenu()
+        {
+            SceneManager.LoadScene(_menuName);
         }
         #endregion
     }
